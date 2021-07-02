b4bd4ccd4e69771958459ec5aae255ada613a4d5
nuxejee <nj.czlex@gmail.com>
2021-07-02 14:16:59 +0700

    //

--- application/models/Digitaltailor_model.php
+++ application/models/Digitaltailor_model.php
@@ -863,7 +863,7 @@ public function addorder($data = '') {
                     // print_r($product->idproduct);exit;
                    $this->db->select('a.*,b.urlImage');
                    $this->db->where('a.idproduct',$product->idproduct);
-                   $this->db->group_by('a.idpditails');
+                   //$this->db->group_by('a.idpditails');
                    $this->db->Join('product_images_ditails as b', 'b.idpditails = a.idpditails', 'left' );
                    $query = $this->db->get_where('product_ditails as a')->result();
 
