d17c82875f930452317edc31a8031767ac00624d
nuxejee <nj.czlex@gmail.com>
2021-07-19 18:58:58 +0700

    voucher_new model ulang

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1922,8 +1922,7 @@ class Admin extends REST_Controller {
         } else {
             $data = array(
                 $this->input->post('keyCodeStaff'),
-                $this->input->post('secret')
-            );
+                $this->input->post('secret'),
 
             $data = $this->admin_model->vouchernew($data);
         }
