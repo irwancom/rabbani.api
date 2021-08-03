008bee91b055794522740bf3cfaed8b0aca910d4
nuxejee <nj.czlex@gmail.com>
2021-08-03 17:25:46 +0700

    update kondisi sms

--- application/models/Pay_model.php
+++ application/models/Pay_model.php
@@ -113,8 +113,14 @@ class Pay_model extends CI_Model {
             $this->db->insert('pay_histories', $data);
         }
         if (!empty($queryx)) {
+              if ($queryx[0]->orderBy==tailordigital) {
+                  $massage = 'tailordigital.id : Silahkan Transfer Rp ' . $totalpay . ', melalui ' . $dataCallBack->bank_code . ' Virtual Account ' . $dataCallBack->account_number . ' a.n ' . $dataCallBack->name . ', Batas Pembayaran 1x24 Jam';
+            $this->sms->SendSms($hp, $massage);
+                  
+              }else {
             $massage = 'rabbani.id : Silahkan Transfer Rp ' . $totalpay . ', melalui ' . $dataCallBack->bank_code . ' Virtual Account ' . $dataCallBack->account_number . ' a.n ' . $dataCallBack->name . ', Batas Pembayaran 1x24 Jam';
             $this->sms->SendSms($hp, $massage);
+              }
 
             $response['status'] = 200;
             $response['error'] = false;
@@ -232,8 +238,15 @@ class Pay_model extends CI_Model {
             }
         }
         if (!empty($query)) {
+            
+            if ($queryx[0]->orderBy==tailordigital) {
+                  $massage = 'tailordigital.id : Silahkan Transfer Rp ' . $totalpay . ', melalui ' . $dataCallBack->bank_code . ' Virtual Account ' . $dataCallBack->account_number . ' a.n ' . $dataCallBack->name . ', Batas Pembayaran 1x24 Jam';
+            $this->sms->SendSms($hp, $massage);
+                  
+              }else {
             $massage = 'rabbani.id : Pembayaran Sebesar Rp ' . $dataCallBack->amount . ' Dari Tagihan Rp ' . $totalpay . ' Melalui ' . $dataCallBack->bank_code . ' Virtual Account Telah Di Terima, Info Pengiriman Slahkan Cek Di Menu Transaksi.';
             $this->sms->SendSms($hp, $massage);
+              }
 
             $response['status'] = 200;
             $response['error'] = false;
