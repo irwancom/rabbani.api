2f15fcff3166f7d24f6f70e53e58c5534c531bf2
nuxejee <49486753+nuxejee@users.noreply.github.com>
2021-09-14 13:27:45 +0700

    ok

--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -1515,8 +1515,8 @@ class Main_model extends CI_Model {
                             
                         }
                     }
-					$this->db->where('idauthuser', $verify[0]->idauthuser);
-                    $this->db->delete('shop_cart');
+					// $this->db->where('idauthuser', $verify[0]->idauthuser);
+     //                $this->db->delete('shop_cart');
 
                  
                     $voucher = $this->db->get_where('voucher_new', array('voucher_code' => $data->voucher))->result();
