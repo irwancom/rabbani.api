b6487c617df7e1a627a33f8341f220b08e1584f0
nuxejee <49486753+nuxejee@users.noreply.github.com>
2021-09-17 11:26:05 +0700

    ok

--- application/models/Main_model.php
+++ application/models/Main_model.php
@@ -4870,7 +4870,7 @@ class Main_model extends CI_Model {
              
              foreach ($user as $y){
                 $this->db->select('a.urlImage');
-                $this->db->group_by('e.idpditails');
+                $this->db->group_by('e.collor');
                 $this->db->where('e.stock>2');
                 $this->db->where('e.delproductditails', 0);
                $this->db->join('product_ditails as e', 'e.idproduct = a.idproduct');
