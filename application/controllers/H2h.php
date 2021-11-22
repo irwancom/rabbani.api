04093829007858377ddd447808c50e4b1c86aaa0
nuxejee <49486753+nuxejee@users.noreply.github.com>
2021-11-22 16:07:51 +0700

    ok

--- application/controllers/H2h.php
+++ application/controllers/H2h.php
@@ -681,7 +681,7 @@ class H2h extends REST_Controller {
         
         // print_r($datay);exit;
         $curl = curl_init();
-        $token = "XGUXhjXbeH2nMxPsUBl6mKSgrLbKmDP8Mdpw9WmLh8JLQBV83jaB5kF9Y18sM4NN";
+        $token = "gEd4VWUNwazHUwVWuS0M9OkwvlXvZYKWQbqgIeExYiqAiwid8d9d8A8kEfxXaiWQ";
         $data = [
             'phone' => $datay->hp,
             'message' => $datay->pesan,
