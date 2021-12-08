20f529ba5136e200177a2c6f3f14a68ee83d055f
irwancom <irhan221087@yahoo.com>
2021-12-08 11:32:04 +0700

    Update Artificial_model.php
    update fixing

--- application/models/Artificial_model.php
+++ application/models/Artificial_model.php
@@ -56,12 +56,14 @@ class Artificial_model extends CI_Model {
                 c.PROVINCE_NAME as province_name, c.CITY_NAME as nameCity, c.ZIP_CODE as postcode, c.CITY_CODE as JNEcode'
             );
             $this->db->where('a.trackingCode!=""');
+            $this->db->where('a.status', 0);
+            $this->db->where('a.statusPay = 1 OR a.statusPay = 4');
+            
             $this->db->join('sensus_people as b', 'b.idpeople = a.idpeople', 'left');
             $this->db->join('sensus as c', 'c.CITY_ID = b.id_city', 'left');
             $this->db->group_by('a.idtransaction');
             $this->db->order_by('a.idtransaction','ASC');
-            $this->db->where('a.status', 0);
-            $this->db->where('a.statusPay = 1 OR a.statusPay = 4');
+            
             // $this->db->or_where('a.statusPay', 4);
             $query = $this->db->get_where('transaction as a')->result();
              //print_r($query);
